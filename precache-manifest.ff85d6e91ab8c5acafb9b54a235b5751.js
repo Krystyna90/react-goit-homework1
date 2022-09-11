fa975f1c1da7a28118a5712b5941bb1461a0144b
ukrbride4u <ukrbride4u@gmail.com>
2022-09-11 19:04:37 +0300

    Updates

---
+++ precache-manifest.ff85d6e91ab8c5acafb9b54a235b5751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d84e1c33fc4b2d8168",
+    "url": "/react-goit-homework1/static/css/main.c924853c.chunk.css"
+  },
+  {
+    "revision": "f5d84e1c33fc4b2d8168",
+    "url": "/react-goit-homework1/static/js/main.f5d84e1c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-goit-homework1/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/react-goit-homework1/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "7976a0859bec6116eaf3",
+    "url": "/react-goit-homework1/static/js/runtime~main.7976a085.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-goit-homework1/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d7e95440de10ada4ab5962107babdf50",
+    "url": "/react-goit-homework1/index.html"
+  }
+];
